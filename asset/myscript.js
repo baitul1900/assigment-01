c513491f7d73367388829a958cd8f09709094e72
Antu <baitulamin1900@gmail.com>
2023-07-28 12:19:38 +0600

    updated

--- asset/myscript.js
+++ asset/myscript.js
@@ -39,9 +39,28 @@ function calculateDiscount(totalpurchase) {
   console.log(`Total Amount: $${totalpurchase}`);
   console.log(`Discount Amount: $${disAmount.toFixed(2)}`);
   console.log(`Discounted Amount: $${finalAmount.toFixed(2)}`);
-};
-
-
-
+}
 
 calculateDiscount(80);
+
+// topic two
+
+/* In this assignment, you will create a JavaScript program that filters even numbers from a given array and displays the filtered numbers on the webpage.
+Output is:-
+
+Original Array: [12, 34, 45, 23, 6, 78, 54, 90]
+Filtered Even Numbers: [12, 34, 6, 78, 54, 90]
+
+Tasks:
+
+Implement a JavaScript function to filter even numbers from the array.
+Display /console.log the filtered even numbers on the code. */
+
+
+
+function filterEvenNumbers(...arr) {
+  let evenNumbers = arr.filter((x) => x % 2 == 0);
+  console.log(evenNumbers);
+}
+
+filterEvenNumbers(12, 34, 45, 23, 6, 78, 54, 90);
